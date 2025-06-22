47b7e258abe0c136499787566c0e47df104a6db4
hermitdesu <ilyas01032000@gmail.com>
2025-06-22 20:12:37 +0300

    user fix

--- app/models/user.py
+++ app/models/user.py
@@ -5,14 +5,13 @@ from typing import Optional, Literal
 class UserIn(BaseModel):
     id: int = Field(default=None, alias="_id")
     name: str = Field(min_length=1, max_length=50)
-    gender: Literal["male", "female"]
 
     model_config = ConfigDict(extra="forbid")
 
 
 class UserDB(UserIn):
-    gender: Literal["male", "female"]
-    language: Literal["ru", "en"]
+    gender: Optional[Literal["male", "female"]] = None
+    language: Optional[Literal["ru", "en"]] = None
     recommendation_method: Optional[Literal["fixed", "kb", "cf"]] = None
     launch_count: int = 0
     current_bundle_version: Optional[int] = None
